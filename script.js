62ebf5e4be90013421942f873ecb303245bc9df5
Alfian Luthfi <77608128+alfianluthfi@users.noreply.github.com>
2026-01-11 11:47:49 +0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const playBtn = document.getElementById("playMusicBtn");
-  const music = document.getElementById("bgMusic");
-
-  playBtn.addEventListener("click", () => {
-    music.play();
-    playBtn.style.display = "none"; // tombol hilang setelah play
-  });
-});
